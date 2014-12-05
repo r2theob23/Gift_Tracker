c5a6f2e004ce8b28e5bf876c55bf6802c1ef7bdc
Robert Smith <r2theob23@gmail.com>
2014-12-05 14:36:17 -0600

    Implemented RecyclerView and created Add Person Activity

--- app/src/main/java/com/robertsmith/gifttracker/MainActivity.java
+++ app/src/main/java/com/robertsmith/gifttracker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.robertsmith.gifttracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Contacts;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,46 +19,47 @@ public class MainActivity extends Activity {
     private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
 
-        // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         ArrayList<Person> people = new ArrayList<Person>();
 
-        for(int i = 0; i<10; i++)
+        for(int i = 0; i<1; i++)
         {
-            people.add(new Person("Name","Budget",10));
+            people.add(new Person("Jessica","$1100",R.drawable.ic_launcher));
         }
 
-        // specify an adapter (see also next example)
         mAdapter = new PeopleAdapter(people);
         mRecyclerView.setAdapter(mAdapter);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_new)
+        {
+            Intent intent = new Intent(this, AddPersonActivity.class);
+            startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/robertsmith/gifttracker/PeopleAdapter.java
+++ app/src/main/java/com/robertsmith/gifttracker/PeopleAdapter.java
@@ -1,5 +1,6 @@
 package com.robertsmith.gifttracker;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,12 +17,8 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder
 {
     private ArrayList<Person> people;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder
     {
-        // each data item is just a string in this case
         public View mView;
 
         public ViewHolder(View v)
@@ -31,24 +28,20 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
     public PeopleAdapter(ArrayList<Person> people)
     {
         this.people = people;
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
     public PeopleAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,int viewType)
     {
-        // create a new view
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, parent, false);
-        // set the view's size, margins, paddings and layout parameters
+
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position)
     {
@@ -57,13 +50,12 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder
         ImageView pic = (ImageView) holder.mView.findViewById(R.id.imageView);
 
         name.setText(people.get(position).getName());
+
         budget.setText(people.get(position).getBudget());
+
         pic.setImageResource(people.get(position).getPic());
-
-
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount()
     {
